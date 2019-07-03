e2f28ca99b8157d1c0c9b50b21d5864759b5814a
jenshaw86 <bloo.j.shaw@gmail.com>
2019-07-03 07:31:19 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,81 +1,62 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+const imageContainer = document.getElementById("dog-image-container");
+const breedsList = document.getElementById("dog-breeds");
+const selectDropdown = document.getElementById("breed-dropdown")
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-const dropdownValue = document.querySelector("select").value
-console.log(dropdownValue);
-const allBreeds = [];
-const dropdown = document.getElementById("breed-dropdown")
-dropdown.addEventListener('change', () => {
-  console.log('changed!');
-  let filteredDogs = allBreeds.filter(dog => {
-    return dog[0] === dropdown.value;
-  })
-  console.log('filtered Dogs', filteredDogs)
-
-})
-// <div id="dog-image-container">
-//   <!-- images here -->
-// </div>
-function fetchDogs(){
-  fetch(imgUrl)
-  .then( res => res.json() )
-  .then( json => json["message"].forEach(dog => addDog(dog)))
-  // addDog()
-}
-function addDog(url){
-  const imageContainer = document.getElementById('dog-image-container')
-  const image = document.createElement('img')
-  image.src = url
-  imageContainer.appendChild(image)
+function main() {
+    fetchDogImages();
+    fetchDogBreeds();
 }
 
-// <ul id="dog-breeds">
-// </ul>
-function fetchBreeds(){
-  fetch(breedUrl)
-  .then( res => res.json() )
-  .then( json => addBreed(json.message))
-}
-function addBreed(breed){
-  // initially, list all dog breeds
-  const breedList = document.getElementById('dog-breeds')
-  for (let dog in breed) {
-    allBreeds.push(dog);
-    const listItem = document.createElement('li')
-    listItem.addEventListener('click', ()=>{listItem.style.color="blue"})
-    listItem.textContent = dog
-    breedList.appendChild(listItem)
-  }
-
-
-
+function fetchDogImages() {
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => displayDogImages(json.message));
 }
 
-//     if (dog.slice(0,1) == dropdownValue) { //dog's first letter is equal to dropdown value )
+function fetchDogBreeds() {
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => displayDogBreeds(json.message))
+}
 
+function displayDogImages(imgUrls) {
+    for(let url of imgUrls) {
+        let image = document.createElement("img");
+        image.src = url;
+        imageContainer.appendChild(image);
+    }
+}
 
+function displayDogBreeds(breeds) {
+    for(let breed in breeds) {
+        addItem(breed);
+    }
 
-// This is how you get dropdown value
-// document.querySelector("select").value
+    selectDropdown.addEventListener("change", () => filterBreeds(breeds))
+}
 
-// for each dropdown option value,
+function filterBreeds(breeds) {
+    while (breedsList.firstChild) {
+        breedsList.removeChild(breedsList.firstChild);
+    }
+    let selection = event.target.value;
+    for(let breed in breeds) {
+        if (breed[0] === selection) {
+            addItem(breed)
+        }
+    }
+}
 
+function addItem(breed) {
+    let item = document.createElement("li");
+    item.textContent = breed;
+    breedsList.appendChild(item);
 
-// <label for="select-breed">Filter Breeds That Start with:</label>
-// <select id="breed-dropdown" name="select-breed">
+    item.addEventListener("click", () => {
+        event.target.style.color = "red"
+    });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// fetchDogs()
-fetchBreeds()
+main()
